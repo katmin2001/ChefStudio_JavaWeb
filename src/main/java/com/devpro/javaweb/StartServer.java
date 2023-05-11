3fe2f68dc43e4a5f0f3d7226cc4ec591cc4aa7ea
Tran Anh <anh.lt@tinasoft.vn>
2023-05-11 14:11:43 +0700

    faster running app

--- src/main/java/com/devpro/javaweb/StartServer.java
+++ src/main/java/com/devpro/javaweb/StartServer.java
@@ -5,9 +5,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class StartServer {
-	
+
 	public static void main(String[] args) {
 		SpringApplication.run(StartServer.class, args);
+		System.out.println("Website is running on: http://localhost:8081");
 	}
-	
+
 }
